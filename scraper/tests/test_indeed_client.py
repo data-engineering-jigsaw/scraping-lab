3f5347bdf882946285e55317e7293bb995083321
Jeff Katzy <jeffreyerickatz@gmail.com>
2022-10-23 13:39:18 -0400

    add all

---
+++ scraper/tests/test_indeed_client.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup as bs
+from src.adapters.indeed_client import *
+import pdb
+
+def test_get_indeed_html():
+    results = get_indeed_html(position = 'data engineer', location = 'United States', start = 1)
+    html_str = '<!DOCTYPE html>\n<html lang="en" dir="ltr">\n<head>\n<meta http-equiv="content-type" content="text/html'
+    assert results.startswith(html_str)
+
+def test_get_job_cards_returns_all_fifteen_jobs_from_page():
+    cards = get_job_cards(position = 'data engineer', location = 'United States', start = 0)
+    assert len(cards) == 15
+
