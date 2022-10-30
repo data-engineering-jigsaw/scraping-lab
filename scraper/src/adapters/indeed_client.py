0322d82e88d00da8b8853e04cabf004239aab18f
Jeff Katzy <jeffreyerickatz@gmail.com>
2022-10-30 12:11:11 -0400

    add with geckodriver

--- scraper/src/adapters/indeed_client.py
+++ scraper/src/adapters/indeed_client.py
@@ -2,8 +2,6 @@ import re
 from bs4 import BeautifulSoup as bs
 import requests
 import pdb
-headers={"User-Agent": "Googlebot"}
-
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,6 +10,9 @@ from selenium.webdriver.common.by import By
 
 
 def get_indeed_html(position = 'data engineer', location = 'United States', start = 0):
+  # !!!!! download firefox and update the below executable_path with path to your geckodriver
+  # !! you can find the geckodriver here https://github.com/mozilla/geckodriver/releases 
+  # download the one that corresponds to your system (mac or linux)
     driver = webdriver.Firefox(executable_path=r'/Users/jeffreykatz/Downloads/geckodriver')
     driver.get("http://www.indeed.com/jobs?q=data+engineer&l=New+York%2C+NY&vjk=67f4c335f087a0eb")
     driver.find_element(By.TAG_NAME, 'html')
